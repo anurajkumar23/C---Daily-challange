2b255eb99640526841a72b5a3c67ed5a5e14b70a
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-06-27 20:21:53 +0530

    Day4

---
+++ GFG/Union of Two Linked Lists/Union of Two Linked Lists.cpp
@@ -0,0 +1,55 @@
+// Given two linked lists, your task is to complete the function makeUnion(), that returns the union list of two linked lists. This union list should include all the distinct elements only and it should be sorted in ascending order.
+
+// Example 1:
+
+// Input:
+// L1 = 9->6->4->2->3->8
+// L2 = 1->2->8->6->2
+// Output: 
+// 1 2 3 4 6 8 9
+// Explaination: 
+// All the distinct numbers from two lists, when sorted forms the list in the output. 
+// Example 2:
+
+// Input:
+// L1 = 1->5->1->2->2->5
+// L2 = 4->5->6->7->1
+// Output: 
+// 1 2 4 5 6 7
+// Explaination: 
+// All the distinct numbers from two lists, when sorted forms the list in the output.
+// Your Task:
+// The task is to complete the function makeUnion() which makes the union of the given two lists and returns the head of the new list.
+
+// Expected Time Complexity: O((N+M)*Log(N+M))
+// Expected Auxiliary Space: O(N+M)
+
+
+class Solution
+{
+    public:
+    struct Node* makeUnion(struct Node* head1, struct Node* head2)
+    {
+        // code here
+        set<int>st;    //creating a set
+        
+        while(head1){
+            st.insert(head1->data);        //inserting data in sorted and remove dupicacy in set
+            head1 = head1->next;
+        }
+        while(head2){
+            st.insert(head2->data);        //inserting data in sorted and remove dupicacy in set
+            head2 = head2->next;
+        }
+        
+        Node *list = new Node(0);     //creating a list for adding those data
+        Node *h = list;    // creating pointer ,pointing to list
+        
+        for(auto i:st){ 
+            list->next = new Node(i);      //traversing though the set and putting all data together
+            list = list->next;
+        }
+        return h->next;
+        
+    }
+};
