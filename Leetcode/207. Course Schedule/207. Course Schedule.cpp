95dfaac2507c5a738803dcf58415f671f235c280
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-07-13 22:10:33 +0530

    Day20

--- Leetcode/207. Course Schedule/207. Course Schedule.cpp
+++ Leetcode/207. Course Schedule/207. Course Schedule.cpp
@@ -27,3 +27,37 @@
 // 0 <= ai, bi < numCourses
 // All the pairs prerequisites[i] are unique.
 
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<int> adj[n];
+        vector<int> indegree(n, 0);
+        vector<int> ans;
+
+        for(auto x: prerequisites){
+            adj[x[0]].push_back(x[1]);
+            indegree[x[1]]++;
+        }
+
+        queue<int> q;
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            auto t = q.front();
+            ans.push_back(t);
+            q.pop();
+
+            for(auto x: adj[t]){
+                indegree[x]--;
+                if(indegree[x] == 0){
+                    q.push(x);
+                }
+            }
+        }
+        return ans.size() == n;
+    }
+};
