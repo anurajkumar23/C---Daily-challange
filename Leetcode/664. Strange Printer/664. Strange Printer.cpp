d27aa538c6a455ac38ed514e62b2496e55c8356a
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-07-30 22:58:57 +0530

    Day37

---
+++ Leetcode/664. Strange Printer/664. Strange Printer.cpp
@@ -0,0 +1,74 @@
+// There is a strange printer with the following two special properties:
+
+// The printer can only print a sequence of the same character each time.
+// At each turn, the printer can print new characters starting from and ending at any place and will cover the original existing characters.
+// Given a string s, return the minimum number of turns the printer needed to print it.
+
+ 
+
+// Example 1:
+
+// Input: s = "aaabbb"
+// Output: 2
+// Explanation: Print "aaa" first and then print "bbb".
+// Example 2:
+
+// Input: s = "aba"
+// Output: 2
+// Explanation: Print "aaa" first and then print "b" from the second place of the string, which will cover the existing character 'a'.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 100
+// s consists of lowercase English letters.
+
+
+
+class Solution {
+public:
+    int n;
+    vector<vector<int>> t;
+    
+    int solve(int l, int r, string& s){
+        if(l == r)
+            return 1;
+        
+        else if(l > r)
+            return 0;
+        
+        if(t[l][r] != -1)
+            return t[l][r];
+
+
+        int i = l+1;
+        while(i <= r && s[i] == s[l])
+            i++;
+
+        if(i == r+1)
+            return 1;
+
+        int normal = 1 + solve(i,r,s);
+
+        int aage_ka = INT_MAX;
+
+        for(int j = i; j <= r; j++){
+            if(s[l] == s[j]){
+                
+                int x = solve(i,j-1,s) + solve(j,r,s);
+                
+                aage_ka = min(aage_ka, x);
+            }
+        }
+
+        return t[l][r] = min(aage_ka, normal);
+    }
+
+    int strangePrinter(string s) {
+        n = s.length() ;
+        
+        t.resize(n,vector<int>(n+1, -1));
+        
+        return solve(0, n-1, s);
+    }
+};
